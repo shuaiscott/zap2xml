cb88a4d0a505898e5d100ba0d14b1ccbe3e7bb72
Scott Carlson <shuaiscott@users.noreply.github.com>
2018-07-03 16:13:44 -0500

    Make XML output in UTF-8

--- entry.sh
+++ entry.sh
@@ -6,7 +6,7 @@
 while :
 do
 	DATE=`date`
-	/zap2xml.pl -u $USERNAME -p $PASSWORD -o /data/$XMLTV_FILENAME $OPT_ARGS
+	/zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
 	echo "Last run time: $DATE"
 	echo "Will run in $SLEEPTIME seconds"
 	sleep $SLEEPTIME
