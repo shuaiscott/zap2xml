8fd268758bab80ad6f67635ba4ba5ca01b7aebf4
Kevin Krotzer <registrations@goatbased.org>
2023-02-07 17:43:23 -0800

    Test for successful completion

--- entry.sh
+++ entry.sh
@@ -5,9 +5,19 @@
 
 while :
 do
-	DATE=`date`
-	/zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
-	echo "Last run time: $DATE"
-	echo "Will run in $SLEEPTIME seconds"
-	sleep $SLEEPTIME
+  DATE=`date`
+  LASTRUN=$('date' +%s)
+  /zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
+  LASTXMLMOD=$(date -r ./data/$XMLTV_FILENAME +%s)
+  if test $LASTXMLMOD -gt $LASTRUN
+  then
+      echo "Last run time: $DATE"
+      echo "Will run in $SLEEPTIME seconds"
+      sleep $SLEEPTIME
+  else
+      echo "Last run time: $DATE"
+      echo "Did not complete successfully"
+      echo "Pausing for 30 seconds and trying again"
+      sleep 30
+  fi
 done
