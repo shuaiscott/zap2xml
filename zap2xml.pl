afc9c62f7fde537ccb0953dfc77afef484e116dd
travis-mcginley <travis.o.mcginley@gmail.com>
2025-07-19 09:26:38 -0400

    fix: use orbebb instead of gapzap

--- zap2xml.pl
+++ zap2xml.pl
@@ -265,7 +265,7 @@ if (defined($options{z})) {
       my $zstart = substr($ms, 0, -3);
       $params = "?time=$zstart&timespan=$gridHours&pref=$zapPref&";
       $params .= &getZapGParams();
-      $params .= '&TMSID=&AffiliateID=gapzap&FromPage=TV%20Grid';
+      $params .= '&TMSID=&AffiliateID=orbebb&FromPage=TV%20Grid';
       $params .= '&ActivityID=1&OVDID=&isOverride=true';
       $rs = &getURL($urlRoot . "api/grid$params",1);
       last if ($rs eq '');
@@ -896,7 +896,7 @@ sub getZapParams {
   $phash{countryCode} = $country;
   $phash{headendId} = $lineupId;
   $phash{device} = $device;
-  $phash{aid} = 'gapzap';
+  $phash{aid} = 'orbebb';
   return %phash;
 }
 
